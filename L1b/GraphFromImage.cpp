009d6f1252760f0a7f2b4fca4cfc919979bdc5c1
MelvynHerzig <34660483+MelvynHerzig@users.noreply.github.com>
2020-09-16 14:55:25 +0200

    Exo2 idx,x,y,V

--- L1b/GraphFromImage.cpp
+++ L1b/GraphFromImage.cpp
@@ -7,54 +7,53 @@
  */
 #include "GraphFromImage.h"
 
-GraphFromImage::GraphFromImage(const bitmap_image& i) : image(i) {
-/****
-*
-*  A IMPLEMENTER
-*
-****/
+GraphFromImage::GraphFromImage (const bitmap_image &i) : image(i)
+{
+
 }
 
 // throws std::out_of_range
-GraphFromImage::Iterable GraphFromImage::adjacent(int v) const {
-/****
-*
-*  A IMPLEMENTER
-*
-****/
+GraphFromImage::Iterable GraphFromImage::adjacent (int v) const
+{
+   Iterable adj;
+
+   unsigned vPx, vPy;
+
+   vPx = x(v);
+   vPy = y(v);
+
+   if(vPx > 0 && )
+
+
 }
 
 // throws std::out_of_range
-int GraphFromImage::idx(int x, int y) const {
-/****
-*
-*  A IMPLEMENTER
-*
-****/
+int GraphFromImage::idx (int x, int y) const
+{
+   if(x < 0 || x >= image.width() || y < 0 || y >= image.height())
+   {
+      throw std::out_of_range("Coordonnees invalides");
+   }
+   return y * image.width() + x;
 }
 
 // throws std::out_of_range
-int GraphFromImage::x(int idx) const {
-/****
-*
-*  A IMPLEMENTER
-*
-****/
+int GraphFromImage::x (int idx) const
+{
+   if(idx >= V() || idx < 0) throw std::out_of_range("Index invalide");
+
+   return idx % image.width();
 }
 
 // throws std::out_of_range
-int GraphFromImage::y(int idx) const {
-/****
-*
-*  A IMPLEMENTER
-*
-****/
+int GraphFromImage::y (int idx) const
+{
+   if(idx >= V() || idx < 0) throw std::out_of_range("Index invalide");
+
+   return idx / image.width();
 }
 
-int GraphFromImage::V() const {
-/****
-*
-*  A IMPLEMENTER
-*
-****/
+int GraphFromImage::V () const
+{
+   return image.width() * image.height();
 }
--- L1b/GraphFromImage.h
+++ L1b/GraphFromImage.h
@@ -6,7 +6,7 @@
  * Updated on 8. septembre 2020 by Antoine Rochat
  */
 #ifndef GRAPHFROMIMAGE_H
-#define	GRAPHFROMIMAGE_H
+#define   GRAPHFROMIMAGE_H
 
 #include <list>
 #include <vector>
@@ -14,34 +14,37 @@
 
 #include "bitmap_image.hpp"
 
-class GraphFromImage {
+class GraphFromImage
+{
 // INTERFACE PUBLIQUE A NE PAS MODIFIER
 public:
 
-    // constructeur
-    GraphFromImage(const bitmap_image& i);
+   // constructeur
+   GraphFromImage (const bitmap_image &i);
 
-    typedef std::list<int> Iterable;
+   typedef std::list<int> Iterable;
 
-    // Retourne les sommets adjacents au sommet v, THROWS std::out_of_range
-    Iterable adjacent(int v) const;
+   // Retourne les sommets adjacents au sommet v, THROWS std::out_of_range
+   Iterable adjacent (int v) const;
 
-    // Retourne le nombre de sommets
-    int V() const;
+   // Retourne le nombre de sommets
+   int V () const;
 
-    // Méthodes permettants de passer de x/y à idx et vice-versa
-    // THROWS std::out_of_range
+   // Méthodes permettants de passer de x/y à idx et vice-versa
+   // THROWS std::out_of_range
 
-    // Index du sommet représentant le pixel a la position x, y
-    int idx(int x, int y) const;
-    // Coordonnée x du sommet idx
-    int x(int idx) const;
-    // Coordonnée y du sommet idx
-    int y(int idx) const;
+   // Index du sommet représentant le pixel a la position x, y
+   int idx (int x, int y) const;
+
+   // Coordonnée x du sommet idx
+   int x (int idx) const;
+
+   // Coordonnée y du sommet idx
+   int y (int idx) const;
 
 // ELEMENTS PRIVES: LIBRE D'AJOUTER DES ELEMENTS
 private:
-    const bitmap_image& image;
+   const bitmap_image &image;
 /****
 *
 *  AJOUTEZ ICI VOS ELEMENTS PRIVES
@@ -49,4 +52,4 @@ private:
 ****/
 };
 
-#endif	/* GRAPHFROMIMAGE_H */
+#endif   /* GRAPHFROMIMAGE_H */
